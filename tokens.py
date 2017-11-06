cf540900794d8064e478f026fae1b8cedab8b321
Thiabaud Engelbrecht <thiabaud.engelbrecht@gmail.com>
2017-11-06 12:42:41 -0800

    split main across 6 files to help with organisation.

---
+++ tokens.py
@@ -0,0 +1,22 @@
+SET, ID, LPAREN, RPAREN, SEMI, EOF = "SET", \
+"ID", "LPAREN", "RPAREN", "SEMI", "EOF"
+DEFUN, COLON, ARROW, COMMA = "DEFUN", "COLON", "ARROW", "COMMA"
+BEGIN, END = "BEGIN", "END"
+
+OP = "OP"
+STR = "STR"
+SELECT, FROM, WHERE = "SELECT", "FROM", "WHERE"
+IN, CONTAINS = "IN", "CONTAINS"
+STAR = "STAR"
+LIST = "LIST"
+ECHO = "ECHO"
+
+class Token(object):
+    def __init__(self, token_type, token_value):
+        # token types listed at top of file
+        self.type = token_type
+        # token values: e.g. MATH412
+        self.value = token_value
+    def __str__(self):
+        return "Token(%s, %s)" % (self.type, repr(self.value))
+    __repr__ = __str__
